93fddde1a7d555580949f4401813049d90974b60
Nacho <i.sotomoreno@gmail.com>
2023-07-12 16:20:37 +0200

    Actualizamos...

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,9 +1,33 @@
-def hello_gcs(event, context):
-    """Triggered by a change to a Cloud Storage bucket.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-    print("HOLAAA FUNCIONAAA VAMOSSS!!!!")
-    file = event
-    print(f"Processing file: {file['name']}.")
+from google.cloud import storage
+from google.cloud import datastore
+import json
+
+
+def write_database(event, context):
+    # Obtener el nombre del archivo JSON creado en Cloud Storage
+    bucket_name = 'thebridge_ej_gcp'  # Cambiar el nombre del bucket aquí
+    file_name = event['name']
+
+    # Crear una instancia del cliente de Cloud Storage
+    storage_client = storage.Client()
+
+    # Obtener el objeto JSON desde Cloud Storage
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    json_data = blob.download_as_text()
+
+    # Convertir el JSON a un diccionario de Python
+    data = json.loads(json_data)
+
+    # Guardar los datos en Datastore
+    datastore_client = datastore.Client()
+    kind = 'users'  # Cambiar el nombre del tipo de entidad aquí
+
+    # Crear una nueva entidad
+    entity = datastore.Entity(datastore_client.key(kind))
+    entity.update(data)
+
+    # Guardar la entidad en Datastore
+    datastore_client.put(entity)
+
+    return 'Datos guardados en Datastore'
---
+++ cloud_run/web.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+from google.cloud import storage, datastore
+import json
+import time
+
+app = Flask(__name__)
+
+# Configura la conexión a Google Cloud Storage
+storage_client = storage.Client()
+bucket_name = 'thebridge_ej_gcp'  # Reemplaza con el nombre de tu bucket en Google Cloud Storage
+bucket = storage_client.bucket(bucket_name)
+
+# Configura la conexión a Datastore
+datastore_client = datastore.Client()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Obtiene los datos ingresados en el formulario
+        id = int(request.form['id'])  # Convierte el campo 'ID' a un número entero
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        fecha = request.form['fecha']
+
+        # Crea el objeto JSON con los datos del usuario
+        usuario = {
+            "ID": id,
+            "Nombre": nombre,
+            "Correo electrónico": correo,
+            "Fecha de registro": fecha
+        }
+
+        # Genera un nombre único para el archivo JSON utilizando la fecha y hora actual
+        timestamp = int(time.time())  # Obtiene la marca de tiempo actual en segundos
+        file_name = f'datos_{timestamp}.json'  # Nombre del archivo JSON
+
+        # Guarda el objeto JSON en Google Cloud Storage
+        blob = bucket.blob(file_name)
+        blob.upload_from_string(data=json.dumps(usuario), content_type='application/json')
+
+        time.sleep(4)
+        # Redirecciona a la misma página para recargar
+        return redirect(url_for('index'))
+
+    # Obtiene todos los usuarios de Datastore
+    users = []
+    query = datastore_client.query(kind='users')
+    docs = query.fetch()
+    for doc in docs:
+        users.append(doc)
+
+    return render_template('index.html', users=users)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
